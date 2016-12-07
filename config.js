1fb33cb8daa4fb3969a5a71e1b55d574f067930f
laurent <htd.laurent@gmail.com>
2016-12-07 12:42:47 +0100

    new issim with config added in lib

--- config.js
+++ config.js
@@ -1,7 +1,10 @@
+if (typeof(APPLIS) == "undefined") {
+    var APPLIS = [];
+}
+if (typeof(MODELS) == "undefined") {
+    var MODELS = [];
+}
+if (typeof(LIBS) == "undefined") {
+    var LIBS = [];
+}
 var USER = { userid: '168', password: 'JHGgcz45JKilmzknzelf65ddDftIO98P' };
-
-var MODELS = [];
-
-var APPLIS = [];
-
-var LIBS = [];
